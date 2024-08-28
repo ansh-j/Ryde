87353b1134acb466d5c1ac53d52cabf9992f403d
Ansh Jasrotia <anshvardhanavj@gmail.com>
2024-08-28 19:48:21 +0530

    inital commits

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>Profle</Text>
+    </SafeAreaView>)
+}
+
+export default Profile;
